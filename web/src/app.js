d7d3eaa3e94c041abea42afb18dea46dbb59ae23
Viola Máté <mtviola9@gmail.com>
2024-05-08 15:45:08 +0200

    vége

--- web/src/app.js
+++ web/src/app.js
@@ -7,3 +7,35 @@
 * Github: https://github.com/matukav
 * Licenc: GNU GPL
 */
+
+const doc = {
+    compBody: document.querySelector("#compBody")
+}
+
+const state = {
+    host: "http://localhost:8000",
+    endpoint: "panaszok"
+}
+
+getComplaints()
+
+async function getComplaints(){
+    const url = state.host + '/' + state.endpoint
+    let response = await fetch(url)
+    let result = await response.json()
+    renderComplaints(result)
+}
+
+function renderComplaints(compList){
+    compList.forEach(comp => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+            <td>${comp.id}</td>
+            <td>${comp.description}</td>
+            <td>${comp.complainant}</td>
+            <td>${comp.product}</td>
+            <td>${comp.type}</td>
+        `
+        doc.compBody.appendChild(tr)
+    })
+}
